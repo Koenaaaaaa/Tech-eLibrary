e545eb5ef71c64402ffdc7db0a4f7ffc172f1b6c
Tumelo Mokoena <Tumelo.Mokoena@dpw.gov.za>
2022-06-07 14:30:09 +0200

    new branch commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { AboutShelfComponent } from './components/about-shelf/about-shelf.component';
 import { OurTeamComponent } from './components/our-team/our-team.component';
 import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -15,13 +19,16 @@ import { HomeComponent } from './components/home/home.component';
     HeaderComponent,
           AboutShelfComponent,
           OurTeamComponent,
-          HomeComponent
+          HomeComponent,
+          ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
